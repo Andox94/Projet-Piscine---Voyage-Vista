5950dd28e6e4e279eb08602980ae57ac81bfe373
AymericGe <aymericgeron@yahoo.fr>
2026-05-27 12:58:36 +0200

    Suppression de l'ancienne version

--- transports.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'config.php';
-
-$dest_id = isset($_GET['dest_id']) ? intval($_GET['dest_id']) : 0;
-
-$stmt = $db->prepare("SELECT * FROM transports WHERE destination_id = ?");
-$stmt->execute([$dest_id]);
-echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
-?>
